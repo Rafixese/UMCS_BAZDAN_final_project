cace4d7e86cabd0f8cedf52225fa390b761b1e32
Rafał Hrabia <rafixese@gmail.com>
2020-06-08 18:06:02 +0200

    Filled with data 7/10 tables

--- skrypt.sql
+++ skrypt.sql
@@ -33,7 +33,7 @@ DROP TABLE item_orders CASCADE CONSTRAINTS;         -- Usuniêcie tabeli item_ord
 CREATE TABLE contract_types
 (
     id              NUMBER,
-    contract_name   VARCHAR(30)             NOT NULL,
+    contract_name   VARCHAR(50)             NOT NULL,
     
     CONSTRAINT      contract_types_pk       PRIMARY KEY(id),
     CONSTRAINT      contract_types_name_uq  UNIQUE (contract_name)
@@ -67,7 +67,7 @@ CREATE TABLE contracts
     signed_date     DATE            NOT NULL,
     expires_date    DATE            NULL,
     salary          NUMBER(7,2)     NOT NULL,
-    job_title       VARCHAR(30)     NOT NULL,
+    job_title       VARCHAR(70)     NOT NULL,
     
     CONSTRAINT      contracts_pk    PRIMARY KEY (id),
     CONSTRAINT      contracts_fk1   FOREIGN KEY (contract_type) REFERENCES contract_types (id)
@@ -344,6 +344,517 @@ CREATE TABLE item_orders
     CONSTRAINT item_orders_pk_fk1   FOREIGN KEY (service_id) REFERENCES services (id),
     CONSTRAINT item_orders_pk_fk2   FOREIGN KEY (product_category) REFERENCES product_categories (id)
 );
+
+/*============================================*/
+/*             Usuniêcie sekwencji            */
+/*============================================*/
+
+DROP SEQUENCE contract_types_seq;
+DROP SEQUENCE contracts_seq;
+DROP SEQUENCE employees_seq;
+DROP SEQUENCE customers_seq;
+DROP SEQUENCE service_types_seq;
+DROP SEQUENCE status_types_seq;
+DROP SEQUENCE services_seq;
+DROP SEQUENCE pricings_seq;
+DROP SEQUENCE product_categories_seq;
+DROP SEQUENCE item_orders_seq;
+
+/*============================================*/
+/*            Utworzenie sekwencji            */
+/*============================================*/
+
+CREATE SEQUENCE contract_types_seq;
+CREATE SEQUENCE contracts_seq;
+CREATE SEQUENCE employees_seq;
+CREATE SEQUENCE customers_seq;
+CREATE SEQUENCE service_types_seq;
+CREATE SEQUENCE status_types_seq;
+CREATE SEQUENCE services_seq;
+CREATE SEQUENCE pricings_seq;
+CREATE SEQUENCE product_categories_seq;
+CREATE SEQUENCE item_orders_seq;
+
+/*============================================*/
+/*             Uzupe³nienie danymi            */
+/*============================================*/
+
+/*
+    Uzupe³nienie tabeli CONTRACT_TYPES przyk³adowymi danymi
+*/
+
+INSERT INTO contract_types  (id, contract_name) 
+VALUES (contract_types_seq.nextval,'Praktyki');
+
+INSERT INTO contract_types  (id, contract_name) 
+VALUES (contract_types_seq.nextval,'Sta¿');
+
+INSERT INTO contract_types  (id, contract_name) 
+VALUES (contract_types_seq.nextval,'Umowa o dzie³o');
+
+INSERT INTO contract_types  (id, contract_name) 
+VALUES (contract_types_seq.nextval,'Umowa o zlecenie');
+
+INSERT INTO contract_types  (id, contract_name) 
+VALUES (contract_types_seq.nextval,'Umowa o pracê na czas okreœlony');
+
+INSERT INTO contract_types  (id, contract_name) 
+VALUES (contract_types_seq.nextval,'Umowa o pracê na czas nieokreœlony');
+
+INSERT INTO contract_types  (id, contract_name) 
+VALUES (contract_types_seq.nextval,'B2B');
+
+INSERT INTO contract_types  (id, contract_name) 
+VALUES (contract_types_seq.nextval,'Umowa agencyjna');
+
+INSERT INTO contract_types  (id, contract_name) 
+VALUES (contract_types_seq.nextval,'Kontrakt mened¿erski');
+
+INSERT INTO contract_types  (id, contract_name) 
+VALUES (contract_types_seq.nextval,'Umowa na okres próbny');
+
+/*
+    Uzupe³nienie tabeli CONTRACTS przyk³adowymi danymi
+*/
+
+INSERT INTO contracts  (id, contract_type, signed_date, expires_date, salary, job_title) 
+VALUES (
+    contracts_seq.nextval, 
+    6, 
+    TO_DATE('15-01-2020', 'DD-MM-YYYY'),
+    null,
+    5672,
+    'W³aœciciel'
+);
+
+INSERT INTO contracts  (id, contract_type, signed_date, expires_date, salary, job_title) 
+VALUES (
+    contracts_seq.nextval, 
+    6, 
+    TO_DATE('20-01-2020', 'DD-MM-YYYY'),
+    null,
+    4245,
+    'Kierownik dzia³u napraw komputerów osobistych'
+);
+
+INSERT INTO contracts  (id, contract_type, signed_date, expires_date, salary, job_title) 
+VALUES (
+    contracts_seq.nextval, 
+    6, 
+    TO_DATE('21-01-2020', 'DD-MM-YYYY'),
+    null,
+    4245,
+    'Kierownik dzia³u napraw telefonów komórkowych'
+);
+
+INSERT INTO contracts  (id, contract_type, signed_date, expires_date, salary, job_title) 
+VALUES (
+    contracts_seq.nextval, 
+    6, 
+    TO_DATE('27-01-2020', 'DD-MM-YYYY'),
+    null,
+    3570,
+    'Starszy serwisant'
+);
+
+INSERT INTO contracts  (id, contract_type, signed_date, expires_date, salary, job_title) 
+VALUES (
+    contracts_seq.nextval, 
+    6, 
+    TO_DATE('03-02-2020', 'DD-MM-YYYY'),
+    null,
+    3620,
+    'Starszy serwisant'
+);
+
+INSERT INTO contracts  (id, contract_type, signed_date, expires_date, salary, job_title) 
+VALUES (
+    contracts_seq.nextval, 
+    7, 
+    TO_DATE('10-02-2020', 'DD-MM-YYYY'),
+    null,
+    3800,
+    'Starszy serwisant'
+);
+
+INSERT INTO contracts  (id, contract_type, signed_date, expires_date, salary, job_title) 
+VALUES (
+    contracts_seq.nextval, 
+    5, 
+    TO_DATE('15-02-2020', 'DD-MM-YYYY'),
+    TO_DATE('15-02-2021', 'DD-MM-YYYY'),
+    3015,
+    'M³odszy serwisant'
+);
+
+INSERT INTO contracts  (id, contract_type, signed_date, expires_date, salary, job_title) 
+VALUES (
+    contracts_seq.nextval, 
+    5, 
+    TO_DATE('25-02-2020', 'DD-MM-YYYY'),
+    TO_DATE('25-02-2021', 'DD-MM-YYYY'),
+    3107,
+    'M³odszy serwisant'
+);
+
+INSERT INTO contracts  (id, contract_type, signed_date, expires_date, salary, job_title) 
+VALUES (
+    contracts_seq.nextval, 
+    2, 
+    TO_DATE('03-03-2020', 'DD-MM-YYYY'),
+    TO_DATE('03-04-2020', 'DD-MM-YYYY'),
+    2500,
+    'Serwisant sta¿ysta'
+);
+
+INSERT INTO contracts  (id, contract_type, signed_date, expires_date, salary, job_title) 
+VALUES (
+    contracts_seq.nextval, 
+    1, 
+    TO_DATE('08-03-2020', 'DD-MM-YYYY'),
+    TO_DATE('08-04-2020', 'DD-MM-YYYY'),
+    0,
+    'Praktykant'
+);
+
+/*
+    Uzupe³nienie tabeli EMPLOYEES przyk³adowymi danymi
+*/
+
+INSERT INTO employees  (id, first_name, last_name, phone, email, pesel, sex, manager_id, contract_id) 
+VALUES (
+    employees_seq.nextval, 
+    'Jorge',
+    'Morrison',
+    '841319326',
+    'jorge.morrison@example.com',
+    '62060601122',
+    'M',
+    null,
+    1
+);
+
+INSERT INTO employees  (id, first_name, last_name, phone, email, pesel, sex, manager_id, contract_id) 
+VALUES (
+    employees_seq.nextval, 
+    'Ronnie',
+    'Cruz',
+    '944304539',
+    'ronnie.cruz@example.com',
+    '51050401122',
+    'M',
+    1,
+    2
+);
+
+INSERT INTO employees  (id, first_name, last_name, phone, email, pesel, sex, manager_id, contract_id) 
+VALUES (
+    employees_seq.nextval, 
+    'Carter',
+    'Hayes',
+    '507650426',
+    'carter.hayes@example.com',
+    '54010801122',
+    'M',
+    1,
+    3
+);
+
+INSERT INTO employees  (id, first_name, last_name, phone, email, pesel, sex, manager_id, contract_id) 
+VALUES (
+    employees_seq.nextval, 
+    'Ashley',
+    'Rice',
+    '865908855',
+    'ashley.rice@example.com',
+    '59070401122',
+    'K',
+    2,
+    4
+);
+
+INSERT INTO employees  (id, first_name, last_name, phone, email, pesel, sex, manager_id, contract_id) 
+VALUES (
+    employees_seq.nextval, 
+    'Irene',
+    'Mcdonalid',
+    '934892675',
+    'irene.mcdonalid@example.com',
+    '82020701122',
+    'K',
+    3,
+    5
+);
+
+INSERT INTO employees  (id, first_name, last_name, phone, email, pesel, sex, manager_id, contract_id) 
+VALUES (
+    employees_seq.nextval, 
+    'Arthur',
+    'Anderson',
+    '290207644',
+    'arthur.anderson@example.com',
+    '74071001122',
+    'M',
+    2,
+    6
+);
+
+INSERT INTO employees  (id, first_name, last_name, phone, email, pesel, sex, manager_id, contract_id) 
+VALUES (
+    employees_seq.nextval, 
+    'Danny',
+    'Andrews',
+    '284347903',
+    'danny.andrews@example.com',
+    '91040201122',
+    'M',
+    3,
+    7
+);
+
+INSERT INTO employees  (id, first_name, last_name, phone, email, pesel, sex, manager_id, contract_id) 
+VALUES (
+    employees_seq.nextval, 
+    'Jason',
+    'Simpson',
+    '125869760',
+    'jason.simpson@example.com',
+    '58020801122',
+    'M',
+    2,
+    8
+);
+
+INSERT INTO employees  (id, first_name, last_name, phone, email, pesel, sex, manager_id, contract_id) 
+VALUES (
+    employees_seq.nextval, 
+    'Alfredo',
+    'Boyd',
+    '408392143',
+    'alfredo.boyd@example.com',
+    '96030901122',
+    'M',
+    3,
+    9
+);
+
+INSERT INTO employees  (id, first_name, last_name, phone, email, pesel, sex, manager_id, contract_id) 
+VALUES (
+    employees_seq.nextval, 
+    'Zoey',
+    'Jimenez',
+    '728055167',
+    'zoey.jimenez@example.com',
+    '91030501122',
+    'K',
+    2,
+    10
+);
+
+/*
+    Uzupe³nienie tabeli STATUS_TYPES przyk³adowymi danymi
+*/
+
+INSERT INTO status_types  (id, name) 
+VALUES (status_types_seq.nextval,'Zg³oszenie przyjête');
+
+INSERT INTO status_types  (id, name) 
+VALUES (status_types_seq.nextval,'Oczekuje na diagnozê');
+
+INSERT INTO status_types  (id, name) 
+VALUES (status_types_seq.nextval,'W diagnozie');
+
+INSERT INTO status_types  (id, name) 
+VALUES (status_types_seq.nextval,'Oczekuje na czêœci zamienne');
+
+INSERT INTO status_types  (id, name) 
+VALUES (status_types_seq.nextval,'Oczekuje na serwis');
+
+INSERT INTO status_types  (id, name) 
+VALUES (status_types_seq.nextval,'W trakcie serwisu');
+
+INSERT INTO status_types  (id, name) 
+VALUES (status_types_seq.nextval,'Oczekuje na kontrole jakoœci');
+
+INSERT INTO status_types  (id, name) 
+VALUES (status_types_seq.nextval,'Kontrola jakoœci serwisu');
+
+INSERT INTO status_types  (id, name) 
+VALUES (status_types_seq.nextval,'Gotowa do wydania');
+
+INSERT INTO status_types  (id, name) 
+VALUES (status_types_seq.nextval,'Zakoñczono');
+
+/*
+    Uzupe³nienie tabeli SERVICE_TYPES przyk³adowymi danymi
+*/
+
+INSERT INTO service_types  (id, name) 
+VALUES (service_types_seq.nextval,'Konserwacja');
+
+INSERT INTO service_types  (id, name) 
+VALUES (service_types_seq.nextval,'Wymiana podzespo³ów');
+
+INSERT INTO service_types  (id, name) 
+VALUES (service_types_seq.nextval,'Naprawa Hardware');
+
+INSERT INTO service_types  (id, name) 
+VALUES (service_types_seq.nextval,'Naprawa Software');
+
+INSERT INTO service_types  (id, name) 
+VALUES (service_types_seq.nextval,'Konfiguracja Software');
+
+INSERT INTO service_types  (id, name) 
+VALUES (service_types_seq.nextval,'Z³o¿enie PC');
+
+INSERT INTO service_types  (id, name) 
+VALUES (service_types_seq.nextval,'Monta¿ os³ony na ekran');
+
+INSERT INTO service_types  (id, name) 
+VALUES (service_types_seq.nextval,'Odbiór elektroodpadów');
+
+INSERT INTO service_types  (id, name) 
+VALUES (service_types_seq.nextval,'Diagnoza');
+
+INSERT INTO service_types  (id, name) 
+VALUES (service_types_seq.nextval,'Doradztwo');
+
+/*
+    Uzupe³nienie tabeli CUSTOMERS przyk³adowymi danymi
+*/
+
+INSERT INTO customers  (id, first_name, last_name, phone, email) 
+VALUES (
+    customers_seq.nextval, 
+    'Eleanor',
+    'Dixon',
+    '669196660',
+    'eleanor.dixon@example.com'
+);
+
+INSERT INTO customers  (id, first_name, last_name, phone, email) 
+VALUES (
+    customers_seq.nextval, 
+    'Armando',
+    'Pena',
+    '597337761',
+    'armando.pena@example.com'
+);
+
+INSERT INTO customers  (id, first_name, last_name, phone, email) 
+VALUES (
+    customers_seq.nextval, 
+    'Frances',
+    'Jones',
+    '260913213',
+    'frances.jones@example.com'
+);
+
+INSERT INTO customers  (id, first_name, last_name, phone, email) 
+VALUES (
+    customers_seq.nextval, 
+    'Minnie',
+    'Turner',
+    '584110839',
+    'minnie.turner@example.com'
+);
+
+INSERT INTO customers  (id, first_name, last_name, phone, email) 
+VALUES (
+    customers_seq.nextval, 
+    'Gabe',
+    'Gonzales',
+    '160086095',
+    'gabe.gonzales@example.com'
+);
+
+INSERT INTO customers  (id, first_name, last_name, phone, email) 
+VALUES (
+    customers_seq.nextval, 
+    'Lance',
+    'Kim',
+    '263232091',
+    'lance.kim@example.com'
+);
+
+INSERT INTO customers  (id, first_name, last_name, phone, email) 
+VALUES (
+    customers_seq.nextval, 
+    'Clifton',
+    'Brooks',
+    '025582263',
+    'clifton.brooks@example.com'
+);
+
+INSERT INTO customers  (id, first_name, last_name, phone, email) 
+VALUES (
+    customers_seq.nextval, 
+    'Sylvia',
+    'Mccoy',
+    '102782683',
+    'sylvia.mccoy@example.com'
+);
+
+INSERT INTO customers  (id, first_name, last_name, phone, email) 
+VALUES (
+    customers_seq.nextval, 
+    'Phyllis',
+    'Hanson',
+    '572248194',
+    'phyllis.hanson@example.com'
+);
+
+INSERT INTO customers  (id, first_name, last_name, phone, email) 
+VALUES (
+    customers_seq.nextval, 
+    'Marjorie',
+    'Holt',
+    '512657032',
+    'marjorie.holt@example.com'
+);
+
+/*
+    Uzupe³nienie tabeli PRODUCT_CATEGORIES przyk³adowymi danymi
+*/
+
+INSERT INTO product_categories  (id, name) 
+VALUES (product_categories_seq.nextval,'Dyski twarde HDD i SSD');
+
+INSERT INTO product_categories  (id, name) 
+VALUES (product_categories_seq.nextval,'Karty graficzne');
+
+INSERT INTO product_categories  (id, name) 
+VALUES (product_categories_seq.nextval,'Procesory');
+
+INSERT INTO product_categories  (id, name) 
+VALUES (product_categories_seq.nextval,'P³yty g³ówne');
+
+INSERT INTO product_categories  (id, name) 
+VALUES (product_categories_seq.nextval,'Pamiêci RAM');
+
+INSERT INTO product_categories  (id, name) 
+VALUES (product_categories_seq.nextval,'Obudowy komputerowe');
+
+INSERT INTO product_categories  (id, name) 
+VALUES (product_categories_seq.nextval,'Zasilacze komputerowe');
+
+INSERT INTO product_categories  (id, name) 
+VALUES (product_categories_seq.nextval,'Ch³odzenia komputerowe');
+
+INSERT INTO product_categories  (id, name) 
+VALUES (product_categories_seq.nextval,'Karty dŸwiêkowe');
+
+INSERT INTO product_categories  (id, name) 
+VALUES (product_categories_seq.nextval,'Napêdy optyczne');
+
+INSERT INTO product_categories  (id, name) 
+VALUES (product_categories_seq.nextval,'Obudowy dysków');
+
+
+
+
+
+
 
 
 
