c98cf601d1c60deb7f876094f1f53a06f5abe211
Rafał Hrabia <rafixese@gmail.com>
2020-06-22 18:07:35 +0200

    Added selects

--- zapytania.sql
+++ zapytania.sql
@@ -67,3 +67,49 @@ SELECT service_id, SUM(quantity * unit_price) AS item_orders_sum FROM item_order
 GROUP BY service_id
 ORDER BY service_id;
 
+-- Lista czêœci dla danego zamówienia
+SELECT product_name, quantity, unit_price FROM item_orders
+WHERE service_id = 7;
+
+-- Us³ugi wykonane przez dzia³ napraw komputerów osobistych
+SELECT service_type, employee_id, customer_id, created_at, status, customer_description, service_description FROM services
+WHERE employee_id IN
+(
+    SELECT employees.id FROM employees WHERE employees.manager_id = 2
+);
+
+-- Pracownicy p³ci ¿eñskiej
+SELECT first_name, last_name FROM employees
+WHERE sex = 'K';
+
+/*
+    Zapytania ³¹cz¹ce (JOIN) dwie tabele
+*/
+
+-- Pensje pracowników
+SELECT employees.first_name, employees.last_name, contracts.salary
+FROM employees INNER JOIN contracts ON employees.contract_id = contracts.id
+WHERE contracts.expires_date IS NULL OR SYSDATE - contracts.expires_date < 0;
+
+-- Stanowiska pracowników
+SELECT employees.first_name, employees.last_name, contracts.job_title
+FROM employees INNER JOIN contracts ON employees.contract_id = contracts.id;
+
+-- Pracownicy zarabiaj¹cy powy¿ej œredniej w firmie
+SELECT employees.first_name, employees.last_name, contracts.job_title, contracts.salary
+FROM employees INNER JOIN contracts ON employees.contract_id = contracts.id
+WHERE (contracts.expires_date IS NULL OR SYSDATE - contracts.expires_date < 0)
+    AND contracts.salary > (SELECT AVG(contracts.salary) FROM contracts);
+    
+-- Pracownicy zarabiaj¹cy poni¿ej œredniej w firmie
+SELECT employees.first_name, employees.last_name, contracts.job_title, contracts.salary
+FROM employees INNER JOIN contracts ON employees.contract_id = contracts.id
+WHERE (contracts.expires_date IS NULL OR SYSDATE - contracts.expires_date < 0)
+    AND contracts.salary < (SELECT AVG(contracts.salary) FROM contracts);
+    
+-- Liczba us³ug danego typu
+SELECT service_types.id, service_types.name, COUNT(services.id) AS services_count
+FROM service_types LEFT JOIN services ON service_types.id = services.service_type
+GROUP BY service_types.id, service_types.name
+ORDER BY service_types.id;
+
