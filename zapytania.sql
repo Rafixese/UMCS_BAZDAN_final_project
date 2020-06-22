0ac8b37e18bd38d47c502d78efc0049a45e5c965
Rafał Hrabia <rafixese@gmail.com>
2020-06-22 15:18:50 +0200

    Added 7 selections

--- zapytania.sql
+++ zapytania.sql
@@ -37,3 +37,33 @@ SELECT signed_date, expires_date, salary, job_title FROM contracts ORDER BY sign
     SELEKCJE
 */
 
+-- Aktywne umowy dzisiejszego dnia
+SELECT contract_type, signed_date, expires_date, salary, job_title  FROM contracts
+WHERE expires_date is NULL OR SYSDATE - expires_date < 0;
+
+-- Pracownicy dzia³u napraw komputerów osobistych (podlegaj¹ kierownikowi tego dzia³u)
+SELECT first_name, last_name FROM employees
+WHERE manager_id = 2;
+
+-- Niezakoñczone us³ugi
+SELECT service_type, employee_id, customer_id, created_at, status, customer_description, service_description FROM services
+WHERE status != 10;
+
+-- Naprawy hardware
+SELECT service_type, employee_id, customer_id, created_at, status, customer_description, service_description FROM services
+WHERE service_type = 3;
+
+-- Us³ugi które przesz³y fazê serwisu a nie maj¹ uzupe³nionej notatki serwisowej
+SELECT service_type, employee_id, customer_id, created_at, status, customer_description FROM services
+WHERE service_description IS NULL;
+
+-- Sumaryczna wycena dla danych us³ug
+SELECT service_id, SUM(value) AS pricing_sum FROM pricings
+GROUP BY service_id
+ORDER BY service_id;
+
+-- Sumaryczny koszt zamówieñ dla danej us³ugi
+SELECT service_id, SUM(quantity * unit_price) AS item_orders_sum FROM item_orders
+GROUP BY service_id
+ORDER BY service_id;
+
